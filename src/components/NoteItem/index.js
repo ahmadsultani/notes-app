ea9b8f5cdb39d2f14a63c99c55d47453f6c60190
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-01-25 10:09:51 +0800

    fixing fonts

--- src/components/NoteItem/index.js
+++ src/components/NoteItem/index.js
@@ -2,10 +2,22 @@ import styles from "./NoteItem.module.css";
 
 export default function NoteItem(props) {
   const { note, onToggleArchive, onDeleteNote } = props;
-  const { id, title, body, archived } = note;
+  const { id, title, body, createdAt, archived } = note;
+
+  const showFormattedDate = (date) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    }
+    return new Date(date).toLocaleDateString("id-ID", options)
+  }
 
   const handleDeleteConfirmation = () => {
-    const confirmation = window.confirm("Are you sure you want to delete this note?");
+    const confirmation = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
     if (confirmation) {
       onDeleteNote(id);
     }
@@ -14,14 +26,21 @@ export default function NoteItem(props) {
   return (
     <li key={id} className={styles.card}>
       <div className={styles.content}>
-        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.contentHeader}>
+          <h2 className={styles.title}>{title}</h2>
+          <p className={styles.date}>{showFormattedDate(createdAt)}</p>
+        </div>
         <p>{body}</p>
       </div>
       <div className={styles.cardButtons}>
         <button type="button" onClick={() => onToggleArchive(id)}>
           {archived ? "Unarchive" : "Archive"}
         </button>
-        <button className={styles.delete} type="button" onClick={handleDeleteConfirmation}>
+        <button
+          className={styles.delete}
+          type="button"
+          onClick={handleDeleteConfirmation}
+        >
           Delete
         </button>
       </div>
